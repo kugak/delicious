08487ce1d5528b110a95c1284e8b2f0b4c04a855
Kuga <contact@kuga.ca>
2022-03-12 12:22:28 -0500

    finish category filters

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Cuisine() {
-  return <div>Cuisine</div>;
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCusine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    console.log(recipes);
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCusine(params.type);
+    console.log(params.type);
+  }, [params.type]); // rerun useEffect when params is updated
+
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Cuisine;
